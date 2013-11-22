eb6d1c418c82d6d0622d3f6f900bd4c25dced317
Sri Sankaran <sri@redhat.com>
2013-11-22 16:43:44 -0500

    Final exam WIP

--- exam/2.js
+++ exam/2.js
@@ -2,19 +2,17 @@ use enron
 c = db.messages
 c.aggregate([
 	// Grab only the from & to
-	{ $project: { from: "$headers.From", toList: "$headers.To"}}
+	{ $project: { _id: "$_id", from: "$headers.From", toList: "$headers.To"}}
 	
   // Remove duplicate from-to pairs
 	, { $unwind: "$toList"}
-	, { $group: { _id: "$from", uniqueRecipients:{ $addToSet: "$toList"}}}
+	, { $group: { _id: {theId:"$_id", from:"$from"}, uniqueRecipients:{ $addToSet: "$toList"}}}
 	
-  // Expand the recipients array
-	, { $unwind: "$uniqueRecipients"}
-	, { $project: { _id:0, from: "$_id", to: "$uniqueRecipients" }}
+//	, { $unwind: "$uniqueRecipients"}
+//	, { $project: { _id:0, from: "$_id", to: "$uniqueRecipients" }}
 
 	// Get a count of number of emails between any given from-to pair
-	, { $group: { _id: {from:"$from", to:"$to"}, count: {$sum: 1}}}
+//	, { $group: { _id: {from:"$from", to:"$toList"}, count: {$sum: 1}}}
 	
-	// Locate documents that have a count value > 1
-	, { $match: { count: {$gt:1}}}
+//	, { $sort: { count: 1}}
   ])
