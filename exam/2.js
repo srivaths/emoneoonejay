57856fffa4fc80e82d29dbae314d905f89b589a3
Sri Sankaran <hashsets@gmail.com>
2013-11-21 23:06:46 -0500

    Exam problem 2 in progress

---
+++ exam/2.js
@@ -0,0 +1,18 @@
+use enron
+c = db.messages
+c.aggregate([
+	// Grab only the from & to
+	{ $project: { from: "$headers.From", toList: "$headers.To"}}
+	
+  // Remove duplicate from-to pairs
+	, { $unwind: "$toList"}
+	, { $group: { _id: "$from", uniqueRecipients:{ $addToSet: "$toList"}}}
+	, { $project: { _id:0, from: "$_id", to: "$uniqueRecipients" }}
+  , { $sort: {from:1}}
+	
+  // Expand the recipients array
+	//, { $unwind: "$uniqueRecipients"}
+	//, { $project: { _id:0, from: "$_id", to: "$uniqueRecipients" }}
+
+	//, { $group: { _id: {from:"$from", to:"$to"}, count: {$sum: 1}}}
+  ])
